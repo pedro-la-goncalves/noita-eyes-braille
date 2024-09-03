33f5c32728ce71b0c03ec03760ee8379f8185c78
Pedro Gonçalves <pedro.goncalves@agnes.com.br>
2024-09-03 15:30:22 -0300

    add README.md

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ import MESSAGES from './input/messages.json' assert { type: 'json' }
 const data = translator.translate(MESSAGES)
 
 // filters for specific data of each message
-// const dataFilteredBySubitem = filter.filterData(data, 'zeroes-as-ones.message-as-binary')
+// const dataFilteredBySubitem = filter.filterData(data, 'zeroes-as-ones.wiki')
 // const dataFilteredBySubitem = filter.filterData(data, 'zeroes-as-ones.wiki.braille-characters')
 const dataFilteredBySubitem = filter.filterData(data)
 
 // filters for data for each message
-// const dataFilteredByMessage = dataFilteredBySubitem['west-1']
+// const dataFilteredByMessage = dataFilteredBySubitem['east-1']
 const dataFilteredByMessage = dataFilteredBySubitem
 
 // generates an output data file
 fileGenerator.generate(dataFilteredByMessage, `output/${new Date().getTime()}`, 'json')
 
 // prints in the console
-console.log(dataFilteredByMessage);
+console.log(JSON.stringify(dataFilteredByMessage));
