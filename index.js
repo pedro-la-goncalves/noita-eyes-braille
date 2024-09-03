3b7efeea66978da1dcbac525cf820bb160567dc2
Pedro Gonçalves <pedro.goncalves@agnes.com.br>
2024-09-03 14:33:08 -0300

    .

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ import filter from './filter.js'
 import fileGenerator from './file-generator.js'
 
 import MESSAGES from './input/messages.json' assert { type: 'json' }
-import MESSAGES_BINARY from './input/messages-binary.json' assert { type: 'json' }
 
+// translates the messages
 const data = translator.translate(MESSAGES)
 
-const filteredData = filter.filterData(data)
-// const filteredData = JSON.stringify(filter.filterData(data))
+// filters for specific data of each message
+// const dataFilteredBySubitem = filter.filterData(data, 'zeroes-as-ones.message-as-binary')
+const dataFilteredBySubitem = filter.filterData(data, 'zeroes-as-ones.wiki.braille-characters')
+// const dataFilteredBySubitem = filter.filterData(data)
 
-fileGenerator.generate(filteredData, `output/${new Date().getTime()}`, 'json')
+// filters for data for each message
+// const dataFilteredByMessage = dataFilteredBySubitem['west-1']
+const dataFilteredByMessage = dataFilteredBySubitem
 
-console.log(filteredData['east-1']);
+// generates an output data file
+fileGenerator.generate(dataFilteredByMessage, `output/${new Date().getTime()}`, 'json')
+
+// prints in the console
+console.log(dataFilteredByMessage);
